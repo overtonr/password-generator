8320602491787d9c1ec050a95301dec3dc0263f8
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-12-23 10:07:51 -0800

    more prompts, function named correctly

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //prompt for pass length (8-128)
 var lowerChar = ['abcdefghijklmnopqrstuvwxyz'];
-var upperChar =['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+var upperChar = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 var numericChar = ['0123456789'];
 var specialChar = ['~!@#$%^&*()-_+={}[]|/;:<>?.'];
 
@@ -19,16 +19,23 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function createPassword(){
+function generatePassword() {
 
   //execution for length of password
   finalPass.length = prompt('How long is your desired password length? please enter a number between 8 and 128')
-    if(finalPass.length < 8 || finalPass.length> 128){
-      alert('Password must be in between 8 and 128 characters long. Please try again');
-    };
+  if (finalPass.length < 8 || finalPass.length > 128) {
+    alert('Password must be in between 8 and 128 characters long. Please try again');
+  };
   //execution for lower
+    var hasLower = confirm('Do you want your password to contain lower case letters? OK if yes, cancel if not');
+    if (hasLower){};
+
   //execution for upper
+    var hasUpper = confirm('Do you want your password to contain upper case letters? OK if yes, cancel if not');
+    if (hasUpper){};
   //execution for special
+  var hasSpecial = confirm('Do you want your password to contain special case letters? OK if yes, cancel if not');
+    if (hasSpecial){};
 };
 
 
