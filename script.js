41977713363da5cac3613b7fa1a9a5293ae11ad6
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-12-23 10:00:00 -0800

    create password breakdown

--- script.js
+++ script.js
@@ -2,27 +2,36 @@
 var lowerChar = ['abcdefghijklmnopqrstuvwxyz'];
 var upperChar =['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 var numericChar = ['0123456789'];
-var specialChar = ['~`!@#$%^&*()-_+={}[]|/;:<>?.'];
+var specialChar = ['~!@#$%^&*()-_+={}[]|/;:<>?.'];
+
+var finalPass = [];
 
 
-
-
-
-
-// Assignment Code
+// linked to button ID in HTML
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button: calls function
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function createPassword(){
+
+  //execution for length of password
+  finalPass.length = prompt('How long is your desired password length? please enter a number between 8 and 128')
+    if(finalPass.length < 8 || finalPass.length> 128){
+      alert('Password must be in between 8 and 128 characters long. Please try again');
+    };
+  //execution for lower
+  //execution for upper
+  //execution for special
+};
+
+
 
 writePassword();
 
